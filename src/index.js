5dbb4d99086c80e17b0077eca1745fc601f41047
Luke Edwards <luke.edwards05@gmail.com>
2018-12-29 00:54:32 -0800

    fix: every <input> has `checked` property

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 export default function (form) {
 	var i=0, j, key, tmp, out={};
-	var rgx = /(file|reset|submit|button)/i;
+	var rgx1 = /(radio|checkbox)/i;
+	var rgx2 = /(file|reset|submit|button)/i;
 
 	while (tmp = form.elements[i++]) {
-		// Ignore unnamed, disabled, or (...rgx) inputs
-		if (!tmp.name || tmp.disabled || rgx.test(tmp.type)) continue;
+		// Ignore unnamed, disabled, or (...rgx2) inputs
+		if (!tmp.name || tmp.disabled || rgx2.test(tmp.type)) continue;
 
 		key = tmp.name;
 
@@ -16,7 +17,7 @@ export default function (form) {
 					out[key].push(tmp.options[j].value);
 				}
 			}
-		} else if (tmp.checked !== void 0) {
+		} else if (rgx1.test(tmp.type)) {
 			if (tmp.checked) {
 				j = out[key];
 				tmp = tmp.value === 'on' || tmp.value;
