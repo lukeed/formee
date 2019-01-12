49ee0a8cb0ec8524598df078151840361f02d6f7
Luke Edwards <luke.edwards05@gmail.com>
2019-01-12 12:48:01 -0800

    chore: add “bind” tests

--- test/index.js
+++ test/index.js
@@ -341,3 +341,150 @@ test('(validate) single', t => {
 
 	t.end();
 });
+
+// ---
+
+test('(bind) mutations', t => {
+	let { form } = toForm();
+	let arr = ['serialize', 'validate', 'onsubmit'];
+
+	arr.forEach(key => {
+		// not true of `onsubmit` in real world, but true here
+		t.is(form[key], undefined, `form.${key} does not initially exist`);
+	});
+
+	let out = bind(form);
+	t.same(out, form, '~> sends back the <form/> value');
+
+	arr.forEach(key => {
+		t.ok(form[key], `form.${key} property added`);
+		t.is(typeof form[key], 'function', '~> is a function');
+	});
+
+	t.end();
+});
+
+test('(bind) serialize', t => {
+	let { form } = toForm({
+		type: 'text',
+		name: 'hello',
+		value: ''
+	}, {
+		type: 'text',
+		name: 'world',
+		value: 'foobar'
+	});
+
+	bind(form);
+
+	t.same(form.serialize(), serialize(form), '~> output identical to serialize()');
+
+	t.end();
+});
+
+test('(bind) validate', t => {
+	let { form } = toForm({
+		type: 'password',
+		name: 'password',
+		value: 'foobar'
+	}, {
+		type: 'password',
+		name: 'confirm',
+		value: 'abc'
+	});
+
+	let rules = {
+		password: myRules.password,
+		confirm: myRules.confirm
+	};
+
+	bind(form, { rules });
+	let all = form.validate();
+
+	t.true(form.isValid !== void 0, 'adds "isValid" property to form');
+	t.false(form.isValid, '~> form marked as invalid');
+
+	t.is(typeof all, 'object', '(all) returns an object');
+	t.same(all, validate(form, rules), '~> identical to validate() call');
+
+	let one = form.validate('password');
+	t.is(typeof one, 'object', '(one) returns an object');
+	t.same(one, validate(form, rules, 'password'), '~> identical to validate() call');
+
+	t.end();
+});
+
+test('(bind) onsubmit', t => {
+	t.plan(23);
+
+	let { form } = toForm({
+		type: 'password',
+		name: 'password',
+		value: 'foobar'
+	}, {
+		type: 'password',
+		name: 'confirm',
+		value: 'abc'
+	});
+
+	let rules = {
+		password: myRules.password,
+		confirm: myRules.confirm
+	};
+
+	function onSubmit(ev) {
+		t.pass('calls "onSubmit" callback');
+		t.is(typeof ev, 'object', '~> receives Event object'); // fake here
+		t.true(ev.errors !== void 0, '~> adds "errors" property to Event');
+		t.same(ev.errors, validate(form, rules), '~~> identical to validate() output');
+		t.same(ev.errors, form.errors, '~> "errors" property is empty object');
+
+		t.true(form.isValid, 'form was marked as valid!');
+		t.true(form.errors !== void 0, '~> adds "errors" property to form');
+		t.same(form.errors, ev.errors, '~> form "errors" matches `ev.errors` value');
+
+		return 'onSubmit output';
+	}
+
+	function onError(ev) {
+		t.pass('calls "onError" callback');
+		t.is(typeof ev, 'object', '~> receives Event object'); // fake here
+		t.true(ev.errors !== void 0, '~> adds "errors" property to Event');
+		t.same(ev.errors, validate(form, rules), '~~> identical to validate() output');
+		t.same(ev.errors, form.errors, '~> "errors" property is empty object');
+
+		t.false(form.isValid, 'form was marked as invalid!');
+		t.true(form.errors !== void 0, '~> adds "errors" property to form');
+		t.same(form.errors, ev.errors, '~> form "errors" matches `ev.errors` value');
+
+		return 'onError output';
+	}
+
+	let event = {
+		preventDefault() {
+			t.pass('calls "preventDefault" on submit event'); //=> x3
+		}
+	};
+
+	// Missing Options
+	// > TypeError: opts.onSubmit is not a function
+	console.log(' '); // spacer
+	bind(form, { rules });
+	t.throws(() => form.onsubmit(event), TypeError, 'throws TypeError if "onSubmit" or "onError" are missing!');
+
+	// Complete Options: Invalid Form
+	console.log(' '); // spacer
+	bind(form, { rules, onSubmit, onError });
+	t.doesNotThrow(() => {
+		let foo = form.onsubmit(event);
+		t.is(foo, 'onError output', '~> returns output from "onError" directly');
+	}, TypeError, 'does not throw TypeError when "onSubmit" and "onError" are defined');
+
+	// Cleanup: Change form values & reset event
+	form.elements.password.value = form.elements.confirm.value = 'password123';
+	delete event.errors; // reset
+
+	// Complete Options: Valid Form
+	let bar = form.onsubmit(event);
+	t.is(bar, 'onSubmit output', '~> returns output from "onSubmit" directly');
+});
