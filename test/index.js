c37b1ce167966c8bbc3376091273e61fc2378ea2
Luke Edwards <luke.edwards05@gmail.com>
2019-01-12 01:25:17 -0800

    chore: add “validate” tests

--- test/index.js
+++ test/index.js
@@ -220,3 +220,124 @@ test('(serialize) specials', t => {
 	});
 	t.end();
 });
+
+// ---
+
+const myRules = {
+	// Optional
+	fullname(val) {
+		if (!val) return true; // because optional
+		return val.trim().length && val.trim().includes(' ') || 'Must be first and last name';
+	},
+	// RegExp rule
+	email: /.+\@.+\..+/,
+	// Function, with custom error messages
+	password(val) {
+		if (!val) return 'Required';
+		return val.length >= 8 || 'Must be at least 8 characters';
+	},
+	// Function, comparing to other value
+	confirm(val, data) {
+		if (!val) return 'Required';
+		return val === data.password || 'Must match your password';
+	}
+};
+
+test('(validate) all', t => {
+	let { form } = toForm({
+		name: 'fullname',
+		type: 'text',
+		value: 'Pocahantas'
+	}, {
+		name: 'email',
+		type: 'email',
+		value: '',
+	}, {
+		name: 'password',
+		type: 'password',
+		value: 'foobar'
+	}, {
+		name: 'confirm',
+		type: 'password',
+		value: 'foo'
+	});
+
+	let out = validate(form, myRules);
+	t.is(typeof out, 'object', '~> returns an object');
+	t.same(out, {
+		email: false,
+		fullname: 'Must be first and last name',
+		password: 'Must be at least 8 characters',
+    confirm: 'Must match your password'
+  });
+
+  t.true('isValid' in form, 'added "isValid" key to form');
+  t.false(form.isValid, '~> is false; had errors');
+
+  form.elements.forEach(elem => {
+	  t.true('isValid' in elem, `(${elem.name}) added "isValid" key to form element`);
+	  t.is(elem.isValid, out[elem.name] == null, `(${elem.name}).isValid = ${out[elem.name] == null}`);
+  });
+
+	t.end();
+});
+
+test('(validate) valid', t => {
+	let { form } = toForm({
+		name: 'fullname',
+		type: 'text',
+		value: ''
+	}, {
+		name: 'email',
+		value: 'asd@asd.com',
+		type: 'email',
+	});
+
+	let out = validate(form, {
+		fullname: myRules.fullname,
+		email: myRules.email,
+	});
+
+	t.is(typeof out, 'object', '~> returns an object');
+	t.same(out, {});
+
+	t.true('isValid' in form, 'added "isValid" key to form');
+  t.true(form.isValid, '~> is true; had no errors');
+
+  ['fullname', 'email'].forEach(name => {
+	  t.true('isValid' in form.elements[name], `(${name}) added "isValid" key to form element`);
+	  t.true(form.elements[name].isValid, `(${name}).isValid = true`);
+  });
+
+	t.end();
+});
+
+test('(validate) single', t => {
+	let { form } = toForm({
+		name: 'fullname',
+		type: 'text',
+		value: ''
+	}, {
+		name: 'email',
+		value: 'asd.com',
+		type: 'email',
+	});
+
+	let foo = validate(form, myRules, 'email');
+	t.is(typeof foo, 'object', '~> returns an object');
+	t.same(foo, { email:false });
+
+	t.false(form.isValid, 'sets form.isValid to false');
+	t.false(form.elements.email.isValid, '~> "email" is invalid');
+	t.is(form.elements.fullname.isValid, undefined, '~> "fullname" is unknown');
+
+	let bar = validate(form, myRules, 'fullname');
+	t.is(typeof bar, 'object', '~> returns an object');
+	t.same(bar, {});
+
+	t.true(form.isValid, 'sets form.isValid to TRUE');
+	t.false(form.elements.email.isValid, '~> "email" remains invalid');
+	t.true(form.elements.fullname.isValid, '~> "fullname" is now valid');
+
+	t.end();
+});
